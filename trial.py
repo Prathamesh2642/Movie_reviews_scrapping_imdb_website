adc472732c794ee3e8ab9b6320168c1ea96e93fe
Prathamesh patil <prathameshpatil2604@gmail.com>
2023-03-28 10:26:45 +0530

    4th commit

---
+++ trial.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+options.headless = False
+driver = webdriver.Chrome(options=options, executable_path=r'C:\\path\\to\\chromedriver.exe')
+driver.get('https://www.imdb.com/title/tt10366206/reviews?ref_=tt_urv')
+print ("Headless Chrome Initialized")
+button = driver.find_element(by=By.CLASS_NAME,value="ipl-load-more__button")
+for i in range(10):
+    button.click()
+    time.sleep(5)
+    button.click()
+    # time.sleep(5)
+    print("done")
