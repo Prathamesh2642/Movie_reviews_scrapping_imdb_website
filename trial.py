65100d01f9404dbc3a3957700714739456e3e460
Prathamesh patil <prathameshpatil2604@gmail.com>
2023-03-28 11:18:29 +0530

    Finding all reviews for the movie

--- trial.py
+++ trial.py
@@ -10,9 +10,16 @@ driver = webdriver.Chrome(options=options, executable_path=r'C:\\path\\to\\chrom
 driver.get('https://www.imdb.com/title/tt10366206/reviews?ref_=tt_urv')
 print ("Headless Chrome Initialized")
 button = driver.find_element(by=By.CLASS_NAME,value="ipl-load-more__button")
-for i in range(10):
-    button.click()
-    time.sleep(5)
-    button.click()
+count=0
+while(True):
+    # button.click()
     # time.sleep(5)
+    # button.click()
+    # time.sleep(5)
+    # button.click()
+    while(True):
+        count+=1
+        time.sleep(5)
+        button.click()
+        print(count)
     print("done")
