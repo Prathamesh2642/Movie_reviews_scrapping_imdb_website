40fa252e28d97f8334f0441a70a32849190ae29c
Prathamesh patil <prathameshpatil2604@gmail.com>
2023-03-28 14:06:31 +0530

    getting source code of page for web scrapping

--- trial.py
+++ trial.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+from bs4 import BeautifulSoup
+import requests
 
 
 def clickloadmore():
     options = webdriver.ChromeOptions()
+    # a=webdriver.Chrome()
     options.add_experimental_option("detach",True)
     options.headless = False
     driver = webdriver.Chrome(options=options, executable_path=r'C:\\path\\to\\chromedriver.exe')
@@ -19,11 +22,19 @@ def clickloadmore():
         # button.click()
         # time.sleep(5)
         # button.click()
-        while(True):
-            count+=1
-            time.sleep(2)
-            button.click()
-            print(count)
+        try:
+            while(True):
+                count+=1
+                time.sleep(2)
+                button.click()
+                print(count)
+        except Exception as e:
+            with open("D:\\Learning only\\Movie_reviews_scrapping_imdb_website\\movies.html","w",encoding='utf-8') as f:
+                f.write(driver.page_source)
+            
+            print(driver.page_source)
+            print(e)
+            break
         print("done")
         
 
